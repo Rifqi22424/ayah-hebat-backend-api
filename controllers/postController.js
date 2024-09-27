10369533fa21756f6dfc9ea03ab6527c6cd011e2
rifqi2242 <rifqimuzakki45@gmail.com>
2024-09-27 08:38:19 +0700

    add user and profile api

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,4 +1,4 @@
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 
@@ -20,341 +20,351 @@ const formatPost = async (post, userId) => {
   return {
     ...post,
     isLikedByMe: !!isLikedByMe,
-    isDislikedByMe: !!isDislikedByMe, 
+    isDislikedByMe: !!isDislikedByMe,
   };
 };
 
 const createPost = async (req, res) => {
-    try {
-      const { body } = req.body;
-      const userId = req.userId;
-
-      if (!body) {
-        return res.status(400).json({ error: 'Body is missing'})
-      }
-
-      const post = await prisma.post.create({
-        data: {
-            userId,
-            body,
-        },
-        include: {
-          user: {
-            select: {
-              id: true,
-              email: true,
-              profile: {
-                select: {
-                  nama: true,
-                  photo: true,
-                }
-              }
-            }
-          },
-          _count: {
-              select: { postLikes: true, postDislikes: true, comments: true }
-          },
-      },
-      });
-
-      const formattedPost = await formatPost(post, userId);
-  
-      res.json({ message: 'Post added successfully.' , data: formattedPost});
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  };
-
-  const editPost = async (req, res) => {
-    try {
-      const { body } = req.body;
-      let { postId } = req.params;
-      const userId = req.userId;
-      
-      postId = parseInt(postId)
-      
-      const findPost = await prisma.post.findUnique({
-        where: { id: postId }
-      });
-
-      if (!findPost) {
-        return res.status(404).json({ error: 'Post not found' });
-      } else if (findPost.userId != userId) {
-        return res.status(203).json({ error: 'Unauthorized access'})
-      }
-
-      const editPost = await prisma.post.update({
-        where: { id: postId },
-        data: {
-            body,
-        },
-        include: {
-          user: {
-            select: {
-              id: true,
-              email: true,
-              profile: {
-                select: {
-                  nama: true,
-                  photo: true,
-                }
-              }
-            }
-          },
-          _count: {
-              select: { postLikes: true, postDislikes: true, comments: true }
-          },
-      },
-      });
-
-      const formattedPost = await formatPost(editPost, userId);
-  
-      res.json({ message: 'Post edited successfully.', data: formattedPost });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  };
-
-  const deletePost = async (req, res) => {
-    try {
-      let { postId } = req.params;
-      const userId = req.userId;
-
-      postId = parseInt(postId);
-      
-      const findPost = await prisma.post.findUnique({
-        where: { id: postId }
-      });
-
-      if (!findPost) {
-        return res.status(404).json({ error: 'Post not found' });
-      } else if (findPost.userId != userId) {
-        return res.status(203).json({ error: 'Unauthorized access'})
-      }
-
-      const deletePost = await prisma.post.delete({
-        where: { id: postId },
-      });
-  
-      res.json({ message: 'Post deleted successfully.' });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  };
-
-  const getAllPosts = async (req, res) => {
-    try {
-      const limit = parseInt(req.query.limit) || 5;
-      const offset = parseInt(req.query.offset) || 0;
-      const userId = req.userId;
-
-      const posts = await prisma.post.findMany({
-        include: {
-            user: {
-              select: {
-                id: true,
-                email: true,
-                profile: {
-                  select: {
-                    nama: true,
-                    photo: true,
-                  }
-                }
-              }
-            },
-            _count: {
-                select: { postLikes: true, postDislikes: true, comments: true }
-            },
-        },
-        orderBy: {
-            postLikes: {
-                _count: 'desc',
-            }            
-        },
-        skip: offset, 
-        take: limit,
-      });
-
-      const formattedPosts = await Promise.all(
-        posts.map(async (post) => {
-          const isLikedByMe = await prisma.postLike.findFirst({
-            where: {
-              postId: post.id,
-              userId,
-            },
-          });
-  
-          const isDislikedByMe = await prisma.postDislike.findFirst({
-            where: {
-              postId: post.id,
-              userId,
-            },
-          });
-  
-          return {
-            ...post,
-            isLikedByMe: !!isLikedByMe, 
-            isDislikedByMe: !!isDislikedByMe, 
-          };
-        })
-      );
-  
-      res.status(200).json(formattedPosts);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  };
-
-  const searchPosts = async (req, res) => {
-    const query  = req.query.query;
-    const limit  = parseInt(req.query.limit) || 5;
-    const offset  = parseInt(req.query.offset) || 0;
+  try {
+    const { body } = req.body;
     const userId = req.userId;
 
-    try {
-        const posts = await prisma.post.findMany({
-            where: {
-                OR: [
-                    { body: { contains: query } },
-                    { user: { username: {contains: query } } },
-                ]
-            },    
-            include: {
-                user: {
-                  select: {
-                    id: true,
-                    email: true,
-                    profile: {
-                      select: {
-                        nama: true,
-                        photo: true, 
-                      }
-                    }
-                  }
-                }
+    if (!body) {
+      return res.status(400).json({ error: "Body is missing" });
+    }
+
+    const post = await prisma.post.create({
+      data: {
+        userId,
+        body,
+      },
+      include: {
+        user: {
+          select: {
+            id: true,
+            email: true,
+            profile: {
+              select: {
+                nama: true,
+                photo: true,
+              },
             },
-            skip: offset,
-            take: limit,
-        });
+          },
+        },
+        _count: {
+          select: { postLikes: true, postDislikes: true, comments: true },
+        },
+      },
+    });
 
-        const formattedPosts = await Promise.all(
-          posts.map(async (post) => {
-            const isLikedByMe = await prisma.postLike.findFirst({
-              where: {
-                postId: post.id,
-                userId,
-              },
-            });
-    
-            const isDislikedByMe = await prisma.postDislike.findFirst({
-              where: {
-                postId: post.id,
-                userId,
-              },
-            });
-    
-            return {
-              ...post,
-              isLikedByMe: !!isLikedByMe, 
-              isDislikedByMe: !!isDislikedByMe, 
-            };
-          })
-        );
+    const formattedPost = await formatPost(post, userId);
 
-        res.status(200).json(formattedPosts);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' });
-
-    }
+    res.json({ message: "Post added successfully.", data: formattedPost });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
+};
 
-  const likePost = async (req, res) => {
-    try {
-        let { postId } = req.params;
-        let userId = req.userId;
+const editPost = async (req, res) => {
+  try {
+    const { body } = req.body;
+    let { postId } = req.params;
+    const userId = req.userId;
 
-        postId = parseInt(postId);
-        userId = parseInt(userId);
+    postId = parseInt(postId);
 
-        const findPost = await prisma.post.findUnique({
-            where: { id: postId }
-          });
-    
-          if (!findPost) {
-            return res.status(404).json({ error: 'Post not found' });
-          } 
+    const findPost = await prisma.post.findUnique({
+      where: { id: postId },
+    });
 
-        const checkLikePost = await prisma.postLike.findFirst({
-            where: { userId, postId },
+    if (!findPost) {
+      return res.status(404).json({ error: "Post not found" });
+    } else if (findPost.userId != userId) {
+      return res.status(203).json({ error: "Unauthorized access" });
+    }
+
+    const editPost = await prisma.post.update({
+      where: { id: postId },
+      data: {
+        body,
+      },
+      include: {
+        user: {
+          select: {
+            id: true,
+            email: true,
+            profile: {
+              select: {
+                nama: true,
+                photo: true,
+              },
+            },
+          },
+        },
+        _count: {
+          select: { postLikes: true, postDislikes: true, comments: true },
+        },
+      },
+    });
+
+    const formattedPost = await formatPost(editPost, userId);
+
+    res.json({ message: "Post edited successfully.", data: formattedPost });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+const deletePost = async (req, res) => {
+  try {
+    let { postId } = req.params;
+    const userId = req.userId;
+
+    postId = parseInt(postId);
+
+    const findPost = await prisma.post.findUnique({
+      where: { id: postId },
+    });
+
+    if (!findPost) {
+      return res.status(404).json({ error: "Post not found" });
+    } else if (findPost.userId != userId) {
+      return res.status(203).json({ error: "Unauthorized access" });
+    }
+
+    const deletePost = await prisma.post.delete({
+      where: { id: postId },
+    });
+
+    res.json({ message: "Post deleted successfully." });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+const getAllPosts = async (req, res) => {
+  try {
+    const limit = parseInt(req.query.limit) || 5;
+    const offset = parseInt(req.query.offset) || 0;
+    const userId = req.userId;
+
+    const posts = await prisma.post.findMany({
+      include: {
+        user: {
+          select: {
+            id: true,
+            email: true,
+            profile: {
+              select: {
+                nama: true,
+                photo: true,
+              },
+            },
+          },
+        },
+        _count: {
+          select: { postLikes: true, postDislikes: true, comments: true },
+        },
+      },
+      orderBy: {
+        postLikes: {
+          _count: "desc",
+        },
+      },
+      skip: offset,
+      take: limit,
+    });
+
+    const formattedPosts = await Promise.all(
+      posts.map(async (post) => {
+        const isLikedByMe = await prisma.postLike.findFirst({
+          where: {
+            postId: post.id,
+            userId,
+          },
         });
 
-        // console.log(checkLikePost.length);
-        
-        if (checkLikePost) {
-            await prisma.postLike.deleteMany({
-                where: { userId, postId },
-            });
-            res.json({ message: 'Post unliked successfully'});
-        } else {
-            await prisma.postLike.create({
-                data: {
-                    postId,
-                    userId
-                }
-            });
-            res.json({ message: 'Post liked successfully'});
-        }
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: 'Internal Server Error'});
-    }
-  };
-
-  const dislikePost = async (req, res) => {
-    try {
-        let { postId } = req.params;
-        let userId = req.userId;
-
-        postId = parseInt(postId);
-        userId = parseInt(userId);
-
-        const findPost = await prisma.post.findUnique({
-            where: { id: postId }
-          });
-    
-          if (!findPost) {
-            return res.status(404).json({ error: 'Post not found' });
-          } 
-
-        const checkLikePost = await prisma.postDislike.findFirst({
-            where: { userId, postId },
+        const isDislikedByMe = await prisma.postDislike.findFirst({
+          where: {
+            postId: post.id,
+            userId,
+          },
         });
 
-        // console.log(checkLikePost.length);
+        return {
+          ...post,
+          isLikedByMe: !!isLikedByMe,
+          isDislikedByMe: !!isDislikedByMe,
+        };
+      })
+    );
 
-        if (checkLikePost) {
-            await prisma.postDislike.deleteMany({
-                where: { userId, postId },
-            });
-            res.json({ message: 'Post undisliked successfully'});
-        } else {
-            await prisma.postDislike.create({
-                data: {
-                    postId,
-                    userId
-                }
-            });
-            res.json({ message: 'Post disliked successfully'});
-        }
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: 'Internal Server Error'});
+    res.status(200).json(formattedPosts);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+const searchPosts = async (req, res) => {
+  const query = req.query.query;
+  const limit = parseInt(req.query.limit) || 5;
+  const offset = parseInt(req.query.offset) || 0;
+  const userId = req.userId;
+
+  try {
+    const posts = await prisma.post.findMany({
+      where: {
+        OR: [
+          { body: { contains: query } },
+          { user: { username: { contains: query } } },
+        ],
+      },
+      include: {
+        user: {
+          select: {
+            id: true,
+            email: true,
+            profile: {
+              select: {
+                nama: true,
+                photo: true,
+              },
+            },
+          },
+        },
+        _count: {
+          select: { postLikes: true, postDislikes: true, comments: true },
+        },
+      },
+      skip: offset,
+      take: limit,
+    });
+
+    const formattedPosts = await Promise.all(
+      posts.map(async (post) => {
+        const isLikedByMe = await prisma.postLike.findFirst({
+          where: {
+            postId: post.id,
+            userId,
+          },
+        });
+
+        const isDislikedByMe = await prisma.postDislike.findFirst({
+          where: {
+            postId: post.id,
+            userId,
+          },
+        });
+
+        return {
+          ...post,
+          isLikedByMe: !!isLikedByMe,
+          isDislikedByMe: !!isDislikedByMe,
+        };
+      })
+    );
+
+    res.status(200).json(formattedPosts);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+const likePost = async (req, res) => {
+  try {
+    let { postId } = req.params;
+    let userId = req.userId;
+
+    postId = parseInt(postId);
+    userId = parseInt(userId);
+
+    const findPost = await prisma.post.findUnique({
+      where: { id: postId },
+    });
+
+    if (!findPost) {
+      return res.status(404).json({ error: "Post not found" });
     }
-  };
 
-module.exports = {createPost, editPost, deletePost, getAllPosts, searchPosts, likePost, dislikePost};
+    const checkLikePost = await prisma.postLike.findFirst({
+      where: { userId, postId },
+    });
+
+    // console.log(checkLikePost.length);
+
+    if (checkLikePost) {
+      await prisma.postLike.deleteMany({
+        where: { userId, postId },
+      });
+      res.json({ message: "Post unliked successfully" });
+    } else {
+      await prisma.postLike.create({
+        data: {
+          postId,
+          userId,
+        },
+      });
+      res.json({ message: "Post liked successfully" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+const dislikePost = async (req, res) => {
+  try {
+    let { postId } = req.params;
+    let userId = req.userId;
+
+    postId = parseInt(postId);
+    userId = parseInt(userId);
+
+    const findPost = await prisma.post.findUnique({
+      where: { id: postId },
+    });
+
+    if (!findPost) {
+      return res.status(404).json({ error: "Post not found" });
+    }
+
+    const checkLikePost = await prisma.postDislike.findFirst({
+      where: { userId, postId },
+    });
+
+    // console.log(checkLikePost.length);
+
+    if (checkLikePost) {
+      await prisma.postDislike.deleteMany({
+        where: { userId, postId },
+      });
+      res.json({ message: "Post undisliked successfully" });
+    } else {
+      await prisma.postDislike.create({
+        data: {
+          postId,
+          userId,
+        },
+      });
+      res.json({ message: "Post disliked successfully" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+module.exports = {
+  createPost,
+  editPost,
+  deletePost,
+  getAllPosts,
+  searchPosts,
+  likePost,
+  dislikePost,
+};
--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,135 +1,160 @@
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 
 const addProfile = async (req, res) => {
-    try {
-      const { nama, namaIstri, namaAnak, namaKuttab, tahunMasukKuttab, bio } = req.body;
-      const userId = req.userId;
+  try {
+    const { nama, namaIstri, namaAnak, namaKuttab, tahunMasukKuttab, bio } =
+      req.body;
+    const userId = req.userId;
 
-      const tahunMasukKuttabInt = parseInt(tahunMasukKuttab);
-  
-      const user = await prisma.user.findUnique({
-        where: { id: userId },
-      });
-  
-      if (!user) {
-        return res.status(404).json({ error: 'User not found' });
-      }
+    const tahunMasukKuttabInt = parseInt(tahunMasukKuttab);
 
-      const photoFilename = req.file ? req.file.filename : null;
-  
-      const profile = await prisma.profile.create({
-        data: {
-          nama,
-          namaIstri,
-          namaAnak,
-          namaKuttab,
-          tahunMasukKuttab: tahunMasukKuttabInt,
-          bio,
-          photo: photoFilename,
-          user: { connect: { id: userId } }, 
-        },
-      });
-  
-      res.json({ message: 'Profile added successfully.' });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+    });
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
     }
-  };
 
-  const editProfile = async (req, res) => {
-    try {
-      const { nama, namaIstri, namaAnak, namaKuttab,tahunMasukKuttab, bio } = req.body;
-      const userId = req.userId;
-  
-      const tahunMasukKuttabInt = parseInt(tahunMasukKuttab);
-  
-      const existingProfile = await prisma.profile.findUnique({
-        where: { userId },
-        include: { user: true }, 
-      });
-  
-      if (!existingProfile) {
-        return res.status(404).json({ error: 'Profile not found' });
-      }
-  
-      if (existingProfile.user.id !== req.userId) {
-        return res.status(403).json({ error: 'Unauthorized access' });
-      }
-  
-      const photoFilename = req.file ? req.file.filename : null;
+    const photoFilename = req.file ? req.file.filename : null;
 
-      const updatedProfile = await prisma.profile.update({
-        where: { userId },
-        data: {
-          nama,
-          namaIstri,
-          namaAnak,
-          namaKuttab,
-          tahunMasukKuttab: tahunMasukKuttabInt,
-          photo: photoFilename,
-          bio,
-        },
-      });
-  
-      res.json({ message: 'Profile updated successfully.', updatedProfile });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
+    const profile = await prisma.profile.create({
+      data: {
+        nama,
+        namaIstri,
+        namaAnak,
+        namaKuttab,
+        tahunMasukKuttab: tahunMasukKuttabInt,
+        bio,
+        photo: photoFilename,
+        user: { connect: { id: userId } },
+      },
+    });
+
+    res.json({ message: "Profile added successfully." });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+const editProfile = async (req, res) => {
+  try {
+    const { nama, namaIstri, namaAnak, namaKuttab, tahunMasukKuttab, bio } =
+      req.body;
+    const userId = req.userId;
+
+    const tahunMasukKuttabInt = parseInt(tahunMasukKuttab);
+
+    const existingProfile = await prisma.profile.findUnique({
+      where: { userId },
+      include: { user: true },
+    });
+
+    if (!existingProfile) {
+      return res.status(404).json({ error: "Profile not found" });
     }
-  };
 
-  const getProfile = async (req, res) => {
-    try {
-      const userId = req.userId;
-  
-      const profile = await prisma.profile.findUnique({
-        where: { userId },
-        select: {
-          id: true,
-          nama: true,
-          bio: true,
-          photo: true,
-          namaIstri: true,
-          namaAnak: true,
-          namaKuttab: true,
-          tahunMasukKuttab: true,
-        },
-      });
-  
-      if (!profile) {
-        return res.status(404).json({ error: 'Profile not found' });
-      }
-  
-      res.json({ profile });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
+    if (existingProfile.user.id !== req.userId) {
+      return res.status(403).json({ error: "Unauthorized access" });
     }
-  };
 
-  const getUserNProfile = async (req, res) => {
-    try {
-      const userId = req.userId;
-  
-      const user = await prisma.user.findUnique({
-        where: { id: userId },
-        include: {
-          profile: true,
-        },
-      });
-  
-      if (!user) {
-        return res.status(404).json({ error: 'User not found' });
-      }
-  
-      res.json({ user });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
+    const photoFilename = req.file ? req.file.filename : null;
+
+    const updatedProfile = await prisma.profile.update({
+      where: { userId },
+      data: {
+        nama,
+        namaIstri,
+        namaAnak,
+        namaKuttab,
+        tahunMasukKuttab: tahunMasukKuttabInt,
+        photo: photoFilename,
+        bio,
+      },
+    });
+
+    res.json({ message: "Profile updated successfully.", updatedProfile });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+const getProfile = async (req, res) => {
+  try {
+    const userId = req.userId;
+
+    const profile = await prisma.profile.findUnique({
+      where: { userId },
+      select: {
+        id: true,
+        nama: true,
+        bio: true,
+        photo: true,
+        namaIstri: true,
+        namaAnak: true,
+        namaKuttab: true,
+        tahunMasukKuttab: true,
+      },
+    });
+
+    if (!profile) {
+      return res.status(404).json({ error: "Profile not found" });
     }
-  };
 
-module.exports = { addProfile, editProfile, getProfile, getUserNProfile };
+    res.json({ profile });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+const getUserPhoto = async (req, res) => {
+  try {
+    const userId = req.userId;
+
+    const profile = await prisma.profile.findUnique({
+      where: { userId },
+      select: {
+        nama: true,
+        photo: true,
+      },
+    });
+
+    if (!profile) {
+      return res.status(404).json({ error: "Profile not found" });
+    }
+
+    res.json({ profile });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+const getUserNProfile = async (req, res) => {
+  try {
+    const userId = req.userId;
+
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+      include: {
+        profile: true,
+      },
+    });
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    res.json({ user });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+module.exports = { addProfile, editProfile, getProfile, getUserNProfile, getUserPhoto };
