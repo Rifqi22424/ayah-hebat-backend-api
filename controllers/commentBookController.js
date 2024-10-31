5320c15877164ea91724344916c2febf13199775
Jamaludin ahmad <jamaludinahmad013@gmail.com>
2024-10-31 10:55:52 +0700

    feat: edit and delete comments

--- controllers/commentBookController.js
+++ controllers/commentBookController.js
@@ -3,11 +3,11 @@ const prisma = new PrismaClient();
 
 const createCommentBook = async (req, res) => {
     try {
-        const idBook = parseInt(req.params.id);
+        const idBook = parseInt(req.params.bookId);
         const userId = parseInt(req.userId);
         const { description } = req.body;
 
-        await prisma.commentBook.create({
+        const commentBook = await prisma.commentBook.create({
             data: {
                 userId: userId,
                 bookId: idBook,
@@ -20,7 +20,7 @@ const createCommentBook = async (req, res) => {
         return res.status(200).json({
             message: "succses create comment",
             data: {
-                description
+                commentBook
             }
         });
 
@@ -74,4 +74,93 @@ const showComment = async (req, res) => {
     }
 }
 
-module.exports = { showComment, createCommentBook }
+const editComment = async (req, res) => {
+    try {
+        const bookId = parseInt(req.params.bookId);
+        const userId = parseInt(req.userId);
+        const commentId = parseInt(req.params.commentId);
+        const { description } = req.body;
+
+        const comment = await prisma.commentBook.findUnique({
+            where: {
+                id: commentId
+            }
+        });
+
+        if(!comment || comment.bookId !== bookId) {
+            return res.status(404).json({
+                message: "Comment not found"
+            });
+        }
+
+        if(comment.userId !== userId) {
+            return res.status(401).json({
+                message: "You are not authorized to edit this comment"
+            });
+        }
+
+        await prisma.commentBook.update({
+            where: {
+                id: commentId
+            },
+            data: {
+                description
+            }
+        });
+
+        return res.status(200).json({
+            message: "success update comment",
+            data: {
+                description
+            }
+        });
+
+    } catch (e) {
+        return res.status(500).json({
+            error: "Internal server error"
+        })
+    }
+}
+
+const deleteComment = async (req, res) => {
+    try {
+        const bookId = parseInt(req.params.bookId);
+        const userId = parseInt(req.userId);
+        const commentId = parseInt(req.params.commentId);
+
+        const comment = await prisma.commentBook.findUnique({
+            where: {
+                id: commentId
+            }
+        });
+
+        if(!comment || comment.bookId !== bookId) {
+            return res.status(404).json({
+                message: "Comment not found"
+            });
+        }
+
+        if(comment.userId !== userId) {
+            return res.status(401).json({
+                message: "You are not authorized to delete this comment"
+            });
+        }
+
+        await prisma.commentBook.delete({
+            where: {
+                id: commentId
+            }
+        });
+
+        return res.status(200).json({
+            message: "success delete comment"
+        });
+
+    } catch (e) {
+        return res.status(500).json({
+            error: "Internal server error"
+        })
+    }
+}
+
+module.exports = { showComment, createCommentBook, editComment, deleteComment }
--- routes/commentBookRoute.js
+++ routes/commentBookRoute.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const {createCommentBook, showComment} = require("./../controllers/commentBookController")
+const {createCommentBook, showComment, editComment, deleteComment} = require("./../controllers/commentBookController")
 
 const router = express.Router();
 
-router.get("/:id", showComment);
-router.post("/:id", createCommentBook);
-
+router.post("/:bookId", createCommentBook);
+router.put("/:bookId/comments/:commentId", editComment);
+router.delete("/:bookId/comments/:commentId", deleteComment);
 module.exports = router;
