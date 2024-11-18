1272b661956c17fc9edd248a92288a6e4076b20e
Jamaludin ahmad <jamaludinahmad013@gmail.com>
2024-11-18 15:59:39 +0700

    update: handle status buku buat admin

--- controllers/peminjamBukuController.js
+++ controllers/peminjamBukuController.js
@@ -6,12 +6,12 @@ const prisma = new PrismaClient();
 // TODO: buat request pinjam buku
 const pinjamBuku = async (req, res) => {
     try {
-        const { bookId, startDate, endDate } = req.body;
+        const { bookId, endDate } = req.body;
         const userId = req.userId;
 
         const book = await prisma.book.findUnique({
             where: {
-                id: bookId
+                id: parseInt(bookId)
             }
         })
 
@@ -36,9 +36,9 @@ const pinjamBuku = async (req, res) => {
         //     })
         // }        
 
-        if(new Date(startDate) >= new Date(endDate)){
+        if(new Date(endDate) < new Date()){
             return res.status(400).json({
-                message: "startDate must ealier than endDate",
+                message: "end date must be greater than today"
             })
         }
 
@@ -46,24 +46,37 @@ const pinjamBuku = async (req, res) => {
             data: {
                 bookId: bookId,
                 userId: userId,
-                startDate: new Date(startDate + "T00:00:00.000Z"),
-                endDate: new Date(endDate + "T00:00:00.000Z")
+                status: "PENDING",
+                startDate: new Date(),
+                endDate: new Date(endDate)
             }
         });
 
         if(!peminjaman){
             return res.status(500).json({
-                message: "failed borrow book"
+                message: "failed lend book"
             });
         }
 
+        const user = await prisma.user.findUnique({
+            where: {
+                id: userId
+            }
+        })
+
         return res.status(202).json({
             message: "success",
-            data: peminjaman
+            data: {
+                idPeminjaman: peminjaman.id,
+                bookName: book.name,
+                endDate: peminjaman.endDate,
+                from: "KUTAB ALFATIH",
+                to: user.username
+            }
         })
     } catch (e) {
         return res.status(500).json({
-            message: e.message
+            error: e.message
         })
     }
 
@@ -100,41 +113,49 @@ const updateStatusBuku = async (req, res) => {
         const idPeminjaman = parseInt(req.params.id);
         const status = req.body.status;
         
-        const book = null;
+        let book = null;
         if(status === "SUDAH_DIAMBIL"){
             book = await prisma.peminjaman.update({
                 where: {
                     id: idPeminjaman
                 },
                 data: {
-                    status: status,
-                    stock: {
-                        decrement: 1
+                    status: "SUDAH_DIAMBIL",
+                    book: {
+                        update: {
+                            stock: {
+                                decrement: 1
+                            }
+                        }
                     }
                 }
             });
         }
 
-        if(status === "DITERIMA"){
+        else if(status === "DITERIMA"){
             book = await prisma.peminjaman.update({
                 where: {
                     id: idPeminjaman
                 },
                 data: {
-                    status: status
+                    status: "DITERIMA"
                 }
             });
         }
 
-        if(status === "SUDAH_DIKEMBALIKAN"){
+        else if(status === "SUDAH_DIKEMBALIKAN"){
             book = await prisma.peminjaman.update({
                 where: {
                     id: idPeminjaman
                 },
                 data: {
-                    status: status,
-                    stock: {
-                        increment: 1
+                    status: "SUDAH_DIKEMBALIKAN",
+                    book: {
+                        update: {
+                            stock: {
+                                increment: 1
+                            }
+                        }
                     }
                 }
             });
@@ -150,8 +171,9 @@ const updateStatusBuku = async (req, res) => {
             data: book
         })
     } catch (e) {
+        console.error(e.message);
         return res.status(500).json({
-            message: e.message
+            error: "failed update status"
         })
     }
 
