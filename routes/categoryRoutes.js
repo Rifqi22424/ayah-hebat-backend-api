2216de2558122ecdd8d898171bcc450ae1925983
Jamaludin ahmad <jamaludinahmad013@gmail.com>
2024-11-19 10:46:04 +0700

    add authorize admin

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { getCategories, createCategory, updateCategory, deleteCategory} = require('../controllers/categoryController');
+const {authorizeAdmin} = require('./../middlewares/authorizationMiddleware')
 const router = express.Router();
 
-router.get('/', getCategories);
-router.post('/', createCategory);
-router.put('/:id', updateCategory);
-router.delete('/:id', deleteCategory);
+router.get('/', authorizeAdmin, getCategories);
+router.post('/', authorizeAdmin, createCategory);
+router.put('/:id', authorizeAdmin, updateCategory);
+router.delete('/:id', authorizeAdmin, deleteCategory);
 
 module.exports = router;
