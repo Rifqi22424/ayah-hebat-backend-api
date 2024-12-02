b4781f37ed16e333f811de4b9339b97b1f91fcfa
rifqi2242 <rifqimuzakki45@gmail.com>
2024-12-02 16:50:40 +0700

    create routes for getMyBookRequest

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,16 +1,16 @@
 const express = require('express');
-const { getBooks, getBookById, createBook, updateBook, deleteBook, createBookRequest, updateBookRequestStatus } = require('../controllers/bookController');
+const { getBooks, getBookById, createBook, updateBook, deleteBook, createBookRequest, updateBookRequestStatus, getMyBookRequests } = require('../controllers/bookController');
 const {uploadPhotoMiddleware} = require('../middlewares/uploadMiddleware');
-const {createCommentBook, editComment, deleteComment} = require("./../controllers/commentBookController")
 const router = express.Router();
+
+router.get('/request', getMyBookRequests);
+router.post('/request', uploadPhotoMiddleware, createBookRequest);
+router.put('/request/:id', updateBookRequestStatus);
 
 router.get('/', getBooks);
 router.get('/:id', getBookById);
 router.post('/', uploadPhotoMiddleware, createBook);
 router.put('/:id', updateBook);
 router.delete('/:id', deleteBook);
-
-router.post('/request', uploadPhotoMiddleware, createBookRequest);
-router.put('/request/:id', updateBookRequestStatus);
 
 module.exports = router;
